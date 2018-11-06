30835315ee532b67247fd44b62b9b6e7f4a7c2b8
boeving <boeving@gmail.com>
2018-11-06 21:31:49 +0800

    add conf nginx view and template

--- apps/onlineconfig/urls/views_urls.py
+++ apps/onlineconfig/urls/views_urls.py
@@ -9,6 +9,9 @@ app_name = 'onlineconfig'
 urlpatterns = [
     # nginx
     #path('',views.index),
-    path('nginx/', views.NginxIndexVies.as_view()),
+    path('nginx/', views.NginxIndexVies.as_view(), name='nginx'),
+    #path('nginx/', views.nginx_index, name='nginx')
+    
+    path('nginx/create_conf', views.create_conf, name='create_conf')
 
 ]
--- apps/onlineconfig/views/nginx.py
+++ apps/onlineconfig/views/nginx.py
@@ -9,6 +9,8 @@ from django.views.generic.detail import DetailView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
+from django.views.decorators.csrf import csrf_protect
 
 from common.utils import get_logger
 
@@ -16,5 +18,30 @@ from common.utils import get_logger
 #logger = get_logger(__name__)
 
 class NginxIndexVies(TemplateView):
-    template_name = "onlineconfig/conf_nginx.html"
+
+        #ctx['server_name'] = request.POST['server_name']
+        #ctx['server_name'] = request.POST['server_name']
+        #ctx['server_name'] = request.POST['server_name']
+
+    template_name = "onlineconfig/confnginx.html"
+    #template_name = "onlineconfig/conf_nginx.html"
+"""
+
+def nginx_index(request):
+    return HttpResponse("<p>lh</p>")
+    #return render(request, 'onlineconfig/confnginx.htm')
+"""
+@csrf_protect
+def create_conf(request):
+    
+    #if request.POST:
+    #    ctx['server_name'] = request.POST['server_name']
+    
+    #receipt = json.dumps({"status": 1, "servername":request.POST})
+    print(3)
+    if request.is_ajax():
+        receipt = json.dumps(request.GET)
+    else:
+        receipt = json.dumps(1)
+    return HttpResponse(receipt)
 
